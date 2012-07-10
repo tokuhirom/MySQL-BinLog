9daacaebbe7f75af5b5da2805cf9994671a54a88
tokuhirom <tokuhirom@gmail.com>
2012-07-11 06:13:09 +0900

    more examples

--- author/inserter.pl
+++ author/inserter.pl
@@ -9,6 +9,14 @@ use DBI;
 
 my $dbh = DBI->connect('dbi:mysql:hostname=127.0.0.1;port=21580;database=foo', 'msandbox', 'msandbox', {RaiseError => 1})
     or die;
+
+{
+    $dbh->do(q{INSERT INTO hoi (id, name) values (1, "HOGEHOGE")});
+    $dbh->do(q{UPDATE hoi SET name="FUGAFUGA" WHERE id=1});
+    $dbh->do(q{DELETE FROM hoi WHERE id=1});
+}
+__END__
+exit;
 for (1..1) {
     {
         my $sth = $dbh->prepare(q{INSERT INTO foo.john (id) values (?)});
---
+++ examples/row-based.pl
@@ -0,0 +1,60 @@
+#!/usr/bin/perl
+use strict;
+use warnings;
+use utf8;
+use 5.010000;
+use autodie;
+use MySQL::BinLog;
+use Pod::Usage;
+use Devel::Peek;
+use Getopt::Long;
+
+GetOptions(
+);
+
+my %table_map;
+my $url = shift or pod2usage;
+my $binlog = MySQL::BinLog->new(MySQL::BinLog::create_transport($url));
+$binlog->connect();
+$binlog->set_position(4);
+say("connected: $binlog");
+while (my $event = $binlog->wait_for_next_event()) {
+    my $type = $event->get_event_type();
+    if ($type eq TABLE_MAP_EVENT) {
+        $table_map{$event->table_id} = $event;
+    } elsif ($type ~~ [WRITE_ROWS_EVENT, UPDATE_ROWS_EVENT, DELETE_ROWS_EVENT]) {
+        my $table_event = $table_map{$event->table_id}
+            or die "Unknown table: " . $event->table_id;
+        my $rows = MySQL::BinLog::Row_event_set->new($event, $table_event);
+        my $iter = $rows->begin();
+        while (my $row = $iter->next()) {
+            if ($type eq WRITE_ROWS_EVENT) {
+                say("INSERT");
+                show_row($row);
+            } elsif ($type eq UPDATE_ROWS_EVENT) {
+                say("UPDATE BEFORE");
+                show_row($row);
+                say("UPDATE AFTER");
+                show_row($iter->next());
+            } elsif ($type eq DELETE_ROWS_EVENT) {
+                say("DELETE");
+                show_row($row);
+            }
+        }
+    }
+}
+
+sub show_row {
+    my $row = shift;
+    my $fields_iter = $row->begin;
+    while (my $field = $fields_iter->next) {
+        printf("       TYPE: %s STR: %s\n", $field->type, $field->as_string);
+    }
+}
+
+__END__
+
+=head1 SYNOPSIS
+
+    % basic-1.pl mysql://msandbox:msandbox@127.0.0.1:21580
+
